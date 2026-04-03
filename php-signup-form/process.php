ac53265af163be9316d3a540111fca2305161fd6
Nancy S <nancy.solano@stu.bcc.cuny.edu>
2026-04-03 11:10:11 -0400

    Initial upload of signup form

---
+++ php-signup-form/process.php
@@ -0,0 +1,11 @@
+<?php
+
+$fullname = $_POST['fullname'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+echo "<h2>Signup Successful</h2>";
+echo "Name: " . $fullname . "<br>";
+echo "Email: " . $email . "<br>";
+
+?>
